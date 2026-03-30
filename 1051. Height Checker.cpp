2f086d17e07fa26b660214a3167f2ab062c2dc3b
Buhai-Maxim <bugayelena806@gmail.com>
2026-03-30 13:58:37 +0300

    Add files via upload

---
+++ 1051. Height Checker.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm> 
+
+class Solution {
+public:
+    int heightChecker(std::vector<int>& heights) {
+        std::vector<int> expected = heights;
+        
+        std::sort(expected.begin(), expected.end());
+        
+        int count = 0;
+        
+        for (int i = 0; i < heights.size(); ++i) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
