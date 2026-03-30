79f357ae5b1d0e5fb94d88c394369bc184a848e2
Buhai-Maxim <bugayelena806@gmail.com>
2026-03-30 14:42:45 +0300

    Add files via upload

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,19 @@
+
+#include <algorithm> 
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if (root == nullptr) {
+            return 0;
+        }
+
+        
+        int leftHeight = maxDepth(root->left);
+        int rightHeight = maxDepth(root->right);
+
+        
+        return std::max(leftHeight, rightHeight) + 1;
+    }
+};
